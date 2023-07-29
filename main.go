44f9dda1bba2b70ad86f18b7f010de5a8ffec542
Ergoold <eran1gold@gmail.com>
2023-07-29 16:21:16 +0300

    Read filenames from the command line

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 )
 
 func main() {
-	processFile(os.Stdin)
+	if len(os.Args) < 2 {
+		processFile(os.Stdin)
+	}
+
+	for _, fileName := range os.Args[1:] {
+		if file, err := os.Open(fileName); err == nil {
+			processFile(file)
+
+			if err = file.Close(); err != nil {
+				report.Warning(err)
+			}
+		} else {
+			report.Warning(err)
+		}
+	}
 }
 
 func processFile(file *os.File) {
 	if stack, err := Mismatches(file); err == nil {
 		WriteDiagnostics(stack, file, os.Stdout)
 	} else {
-		report.Fatal(err)
+		report.Warning(err)
 	}
 }
