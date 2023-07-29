ec35a065ff4ce1775e5044dcbba7b16421aa144b
Ergoold <eran1gold@gmail.com>
2023-07-29 16:30:35 +0300

    Document error reporting procedures

--- report/report.go
+++ report/report.go
@@ -5,11 +5,13 @@ import (
 	"os"
 )
 
+// Fatal prints err to stderr and exits with a non-zero return value.
 func Fatal(err error) {
 	Warning(err)
 	os.Exit(1)
 }
 
+// Warning prints err to stderr without exiting the program.
 func Warning(err error) {
 	_, _ = fmt.Fprintf(os.Stderr, "mismatch: %v\n\n", err)
 }
