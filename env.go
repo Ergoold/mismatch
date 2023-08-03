a8dc736826929c926667c7d7363b93809789dd8b
Ergoold <eran1gold@gmail.com>
2023-08-03 21:27:18 +0300

    Use parse library to parse environment variables

--- env.go
+++ env.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"mismatch/parse"
+	"mismatch/report"
 	"os"
-	"strconv"
 )
 
 var (
 	// BufferSize is the size of the internal read buffer.
-	BufferSize = env("BUFFER_SIZE", strconv.Atoi, 64)
+	BufferSize = env("BUFFER_SIZE", parse.ByteSize, 64)
 
 	// InitialStackDepth is the initial stack depth for keeping track of mismatched parentheses.
-	InitialStackDepth = env("INITIAL_STACK_DEPTH", strconv.Atoi, 4)
+	InitialStackDepth = env("INITIAL_STACK_DEPTH", parse.PositiveInteger, 4)
 )
 
 type parser[T any] func(string) (T, error)
@@ -20,7 +23,18 @@ func env[T any](name string, parser parser[T], defaultValue T) T {
 
 	if parsed, err := parser(value); err == nil {
 		return parsed
+	} else if !errors.Is(err, parse.ErrEmpty) {
+		report.Warning(&envError{name, err})
 	}
 
 	return defaultValue
+}
+
+type envError struct {
+	name string
+	err  error
+}
+
+func (e *envError) Error() string {
+	return fmt.Sprintf("parsing %s: %v", e.name, e.err)
 }
