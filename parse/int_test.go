d2324ff5227c880b85b29de04744ce7d0a1ab93e
Ergoold <eran1gold@gmail.com>
2023-08-03 20:59:54 +0300

    Write tests for parse.NonnegativeInteger

--- parse/int_test.go
+++ parse/int_test.go
@@ -22,10 +22,24 @@ var (
 		{"0x", 0, ErrSyntax},
 		{"x0", 0, ErrSyntax},
 	}
+	nonnegativeIntegerTests = []testCase[int]{
+		{"", 0, ErrEmpty},
+		{"1", 1, nil},
+		{"12", 12, nil},
+		{"01", 0, ErrSyntax},
+		{"0", 0, nil},
+		{"12x", 0, ErrSyntax},
+		{"0x", 0, ErrSyntax},
+		{"x0", 0, ErrSyntax},
+	}
 )
 
 func TestPositiveInteger(t *testing.T) {
 	runTestCases(t, "PositiveInteger", PositiveInteger, positiveIntegerTests)
+}
+
+func TestNonnegativeInteger(t *testing.T) {
+	runTestCases(t, "NonnegativeInteger", NonnegativeInteger, nonnegativeIntegerTests)
 }
 
 func runTestCases[T comparable](t *testing.T, fnName string, fn func(string) (T, error), tests []testCase[T]) {
